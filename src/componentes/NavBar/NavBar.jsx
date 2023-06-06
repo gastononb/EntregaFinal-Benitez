0b2832e8e18a27f93c78d1fab744c2c33bf129e8
Gaston Benitez <gastonbenitez97.gb@gmail.com>
2023-06-06 13:36:47 -0300

    primer-pre-entrega

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -3,12 +3,13 @@ import CartWidget from '../CartWidget/CartWidget'
 const NavBar = () => {
   return (
     <header>
-      <h1 className='logoMenu'>logo</h1>
+      <h1 className='logoMenu'>PadelStore</h1>
         <nav>
           <ul>
+            <li>Paletas</li>
             <li>Remeras</li>
             <li>Pantalones</li>
-            <li>Camperas</li>
+            <li>Calzado</li>
           </ul>
           
         </nav>
